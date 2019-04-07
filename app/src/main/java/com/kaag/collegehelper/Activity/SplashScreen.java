62115d2a57ac4164c481d9925ca3a7cae6166681
Shubhamhack7 <shubhamtiwaricr07@gmail.com>
2019-04-07 13:40:31 +0530

    adding calling Calsses

--- app/src/main/java/com/kaag/collegehelper/Activity/SplashScreen.java
+++ app/src/main/java/com/kaag/collegehelper/Activity/SplashScreen.java
@@ -1,9 +1,15 @@
 package com.kaag.collegehelper.Activity;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.kaag.collegehelper.CommonClasses.ApiCaller;
 import com.kaag.collegehelper.R;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 
 public class SplashScreen extends AppCompatActivity {
 
---
+++ app/src/main/java/com/kaag/collegehelper/CommonClasses/ApiCaller.java
@@ -0,0 +1,78 @@
+package com.kaag.collegehelper.CommonClasses;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+public class ApiCaller extends AsyncTask<ArrayList, String, JSONObject> {
+
+    private final JSONParser jsonParser = new JSONParser();
+
+    private String METHOD = "";
+    private String URL = "";
+    private JSONObject callResult = null;
+
+    public JSONObject makeApiCall(String url, String method, Map<String, String> map) {
+        final ArrayList params = new ArrayList();
+
+        if (!url.isEmpty()) {
+            URL = url;
+        } else {
+            Log.e("ApiCaller", "URl cant be empty");
+            return null;
+        }
+        if (!method.isEmpty()) {
+            METHOD = method;
+        } else {
+            Log.e("ApiCaller", "Method not spacified");
+            return null;
+        }
+        if (map.size() == 0) {
+            Log.e("ApiCaller", "No parameters");
+            return null;
+        }
+        for (Map.Entry data : map.entrySet()){
+            params.add(new BasicNameValuePair(data.getKey().toString(), data.getValue().toString()));
+        }
+        Callable callable = new Callable() {
+            @Override
+            public Object call() throws Exception {
+                return execute(params);
+            }
+        };
+        try {
+            callable.call();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return callResult;
+
+    }
+
+    @Override
+    protected JSONObject doInBackground(ArrayList... arrayLists) {
+
+        ArrayList data=arrayLists[0];
+        JSONObject json = jsonParser.makeHttpRequest(URL,METHOD, data);
+        return json;
+
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    protected void onPostExecute(JSONObject result) {
+        callResult=result;
+    }
+
+
+}
